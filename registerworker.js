9cab7124e4fdf9a3f09c412e290e909c6bc0a99b
Winter-Raven <reallyleximiller@gmail.com>
2021-08-05 18:12:05 -0400

    remove service worker

--- registerworker.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('sw_cache.js')
-            .then(reg => console.log('Service Worker: Registered'))
-            .catch(err => console.log(`Service Worker: Error ${err}`));
-    });
-}
